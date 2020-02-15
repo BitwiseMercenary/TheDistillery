42fb60c00072f5f866e0a87c51a2299c2fec4154
Robert Mercado <robert@remine.com>
2020-02-15 11:31:59 -0500

    testing /docs folder

---
+++ docs/precache-manifest.3b231506a5adf66de83bcc1e119b9d44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12ec03221b6166116b60739facc0a48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c586c55a88e7349ddc4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1620d5feb0c2f8bb8562",
+    "url": "/static/js/2.c5320fc7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c5320fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c586c55a88e7349ddc4",
+    "url": "/static/js/main.887e7d8d.chunk.js"
+  },
+  {
+    "revision": "b217452b5c182342b772",
+    "url": "/static/js/runtime-main.ea157718.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
