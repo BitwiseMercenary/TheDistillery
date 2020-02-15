3a333f76d201710aea528f5e63dcf4167e27c918
Robert Mercado <robert@remine.com>
2020-02-15 11:55:59 -0500

    Updates

---
+++ precache-manifest.e6c4d5808f674d018b5139d308f74faf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "995c9d15333d166e241bc5e723350881",
+    "url": "/TheDistillery/index.html"
+  },
+  {
+    "revision": "f42c9fa7e77054efa42b",
+    "url": "/TheDistillery/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1620d5feb0c2f8bb8562",
+    "url": "/TheDistillery/static/js/2.c5320fc7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/TheDistillery/static/js/2.c5320fc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42c9fa7e77054efa42b",
+    "url": "/TheDistillery/static/js/main.dc22a173.chunk.js"
+  },
+  {
+    "revision": "8a7dc9cc4204026bb9e7",
+    "url": "/TheDistillery/static/js/runtime-main.7f7dc758.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TheDistillery/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b231506a5adf66de83bcc1e119b9d44.js"
+  "/TheDistillery/precache-manifest.e6c4d5808f674d018b5139d308f74faf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TheDistillery/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
