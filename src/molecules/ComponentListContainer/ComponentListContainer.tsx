bff4cb010d9128607a179815d3c680e15eede102
DarkseidOmega <Spartn117.RM@gmail.com>
2021-01-09 15:30:16 -0500

    feat(TheDistillery): update gitignore and bumped styled-components version

--- src/molecules/ComponentListContainer/ComponentListContainer.tsx
+++ src/molecules/ComponentListContainer/ComponentListContainer.tsx
@@ -4,23 +4,20 @@ import {Div} from "../../atoms";
 import {ComponentListContainerProps, ComponentListContainerTheme, ThemeSelector} from "./types";
 import {DivProps} from "../../models/styled-system";
 
-const StyledComponentListContainer: React.FunctionComponent<DivProps & {theme?: any} > = styled(Div)`
-  ${props => {
-    console.log({ ...props });
-    return ThemeSelector[props.theme]
-  }
-  }
-}
+const StyledComponentListContainer: React.FunctionComponent<DivProps & { theme?: any }> = styled(Div)`
+  ${props => ThemeSelector[props.theme]}
 `;
 
 /**
  * Displays children in list
  */
-export const ComponentListContainer = ({ theme =  ComponentListContainerTheme.DEFAULT, ...props }: ComponentListContainerProps) => {
+export const ComponentListContainer = ({
+                                           theme = ComponentListContainerTheme.DEFAULT,
+                                           ...props
+                                       }: ComponentListContainerProps) => {
     /*
         1. get children from props
         2. if a child doesnt have an id, tag it with one generated from the Parents class name
      */
-    console.log({ ...props, theme, loc: "ComponentListContainer"})
-    return <StyledComponentListContainer { ...props } theme={theme}>{props.children}</StyledComponentListContainer>;
+    return <StyledComponentListContainer {...props} theme={theme}>{props.children}</StyledComponentListContainer>;
 };
